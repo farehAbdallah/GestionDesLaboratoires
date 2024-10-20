6231d97af56550817d6f2a11c71c64ac8362b875
farehAbdallah <abdallahfareh0@gmail.com>
2024-10-20 21:37:17 +0100

    Initial commit projet libre

---
+++ Backend-GestionLaboratoires/src/main/java/gi/projetlibre/backendgestionlaboratoires/BackendGestionLaboratoiresApplication.java
@@ -0,0 +1,13 @@
+package gi.projetlibre.backendgestionlaboratoires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGestionLaboratoiresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendGestionLaboratoiresApplication.class, args);
+    }
+
+}
---
+++ Backend-GestionLaboratoires/src/test/java/gi/projetlibre/backendgestionlaboratoires/BackendGestionLaboratoiresApplicationTests.java
@@ -0,0 +1,13 @@
+package gi.projetlibre.backendgestionlaboratoires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGestionLaboratoiresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
