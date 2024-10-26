b87e0a71c4fb74a0e0f339a4e28171a0e0dd6730
farehAbdallah <abdallahfareh0@gmail.com>
2024-10-26 17:51:04 +0100

    implementation de l'architecture microservices en general

---
+++ Backend-GestionLaboratoires/LaboratoireService/src/test/java/net/chabab/laboratoireservice/LaboratoireServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chabab.laboratoireservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoireServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
