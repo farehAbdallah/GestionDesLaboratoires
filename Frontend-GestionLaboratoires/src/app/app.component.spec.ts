50e9f2cdd952a97f576751b10d344db6ff35dfa3
farehAbdallah <abdallahfareh0@gmail.com>
2024-10-31 20:56:32 +0100

    Setup Jest for the frontend(Angular)

--- Frontend-GestionLaboratoires/src/app/app.component.spec.ts
+++ Frontend-GestionLaboratoires/src/app/app.component.spec.ts
@@ -1,35 +1,24 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, FirstComponentComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'Frontend-GestionLaboratoires'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Frontend-GestionLaboratoires');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Frontend-GestionLaboratoires');
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
   });
 });
