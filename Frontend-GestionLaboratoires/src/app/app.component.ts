352a0afc7be7b4292cef409dc75727b20ba4f941
farehAbdallah <abdallahfareh0@gmail.com>
2024-11-29 03:40:35 +0100

    Ajout d'un exemple simple de crud dans frontend

---
+++ Frontend-GestionLaboratoires/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {DashboardLayoutComponent} from './layout/dashboard-layout/dashboard-layout.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, DashboardLayoutComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Frontend-GestionLaboratoires';
+}
