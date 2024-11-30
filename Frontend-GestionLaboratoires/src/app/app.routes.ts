37bc28ec44b371ab22dc023f29268761fc942e3c
farehAbdallah <abdallahfareh0@gmail.com>
2024-11-30 23:30:01 +0100

    Creation de page pour connexion (LogIn UI)

--- Frontend-GestionLaboratoires/src/app/app.routes.ts
+++ Frontend-GestionLaboratoires/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {AccueilComponent} from './components/accueil/accueil.component';
 import {UtilisateursComponent} from './components/utilisateurs/utilisateurs.component';
+import {LoginComponent} from './components/login/login.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'accueil', pathMatch: 'full' },
   {path: 'accueil', component: AccueilComponent, data: { title: 'Accueil' },},
-  {path: 'utilisateurs', component: UtilisateursComponent, data: { title: 'Gestion des Utilisateurs' },}
+  {path: 'utilisateurs', component: UtilisateursComponent, data: { title: 'Gestion des Utilisateurs' },},
+  {path: 'login', component: LoginComponent, data: { title: 'Log In' },}
 ];
